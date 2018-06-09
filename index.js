824b5f34b841a499c8dd33ba47cdb28333960be1
Martin-MacDonald <mmacdo54@caledonian.ac.uk>
2018-06-09 16:23:00 +0100

    Initial commit

---
+++ index.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const app = express();
+
+app.use(bodyParser.json());
+
+const port = process.env.PORT || 3000;
+
+app.get('/shopping', (req, res) => {
+  res.send('Hello World');
+});
+
+app.post('/shopping', (req, res) => {
+  res.send('Put')
+});
+
+app.patch('/shopping', (req, res) => {
+  res.send('Patch');
+});
+
+app.delete('/shopping', (req, res) => {
+  res.send('Deleted');
+});
+
+app.listen(port, () => {
+  console.log(`Listening on port ${port}`);
+});
